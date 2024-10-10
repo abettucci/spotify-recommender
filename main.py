cbb39109fb10002c2c865d7c7f1aff2ab3260561
abettucci <bettucci.agustin@gmail.com>
2024-10-09 21:40:57 -0300

    fix de puerto utilizado

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def logueo_spotify():
      sp_client = spotipy.Spotify(auth_manager=SpotifyOAuth(
                                                   client_id = client_id,
                                                   client_secret = client_secret,
-                                                  redirect_uri = 'https://spotify-recommender-fdgfxtwf3xed9ufqnilaan.streamlit.app/',
+                                                  redirect_uri = 'https://spotify-recommender-fdgfxtwf3xed9ufqnilaan.streamlit.app/callback',
                                                   scope=[# Tracks, albums, artistas guardados en Biblioteca
                                                        "user-library-read",
                                                        # User Info
